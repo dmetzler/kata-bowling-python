5ed63413a0cbc61bf49c2e9c16e4708d0dd0cd9c
Damien Metzler <dmetzler@nuxeo.com>
2021-05-20 12:59:00 +0000

    first steps

--- Bowling.py
+++ Bowling.py
@@ -2,5 +2,11 @@
 class Game:
 
     def __init__(self):
-        pass
+        self._score = 0
+    
+    @property
+    def score(self) -> int:
+        return self._score
 
+    def takedown(self, pins_number: int) -> None:
+        self._score += pins_number
--- test_Bowling.py
+++ test_Bowling.py
@@ -10,3 +10,32 @@ def game():
 def test_can_create_a_game(game):
     assert game is not None
 
+def test_score(game):
+    assert game.score == 0
+
+def test_takedown_pins(game):
+    game.takedown(3)
+    assert game.score == 3
+
+def test_guter_game(game):
+    roll_many(game,20,0)
+    assert game.score == 0
+
+def test_simple_game(game):
+    roll_many(game,20,1)
+    assert game.score == 20
+
+def roll_many(game, times, pins):
+    for _ in range(0,times):
+        game.takedown(pins)
+
+def test_game_with_spare(game):
+    game.takedown(0)
+    game.takedown(0)
+    game.takedown(8)
+    game.takedown(2)
+    game.takedown(3)
+    game.takedown(4)
+    roll_many(game,16,0)
+
+    assert game.score == 20
