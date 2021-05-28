0c4b46688b6323bd63c9206f65e8221d21dc0969
Damien Metzler <dmetzler@nuxeo.com>
2021-05-28 13:35:50 +0000

    Test spare

--- Bowling.py
+++ Bowling.py
@@ -2,11 +2,30 @@
 class Game:
 
     def __init__(self):
-        self._score = 0
-    
+        self._rolls = []
+
+    def is_spare(self,frameIndex):
+        return self.get_frame_score(frameIndex) == 10
+         
+    def get_frame_score(self, frameIndex):
+        first = frameIndex*2
+        second = first+1
+
+        return (self._rolls[first] if first < len(self._rolls) else 0) + (self._rolls[second] if second < len(self._rolls) else 0)
+
     @property
     def score(self) -> int:
-        return self._score
+        score = 0
+        rollIndex = 0
+        for frameIndex in range(0,10):
+            if self.is_spare(frameIndex):
+                bonus_roll = (frameIndex+1)*2
+                score += 10 + (self._rolls[bonus_roll] if bonus_roll < len(self._rolls) else 0)
+                rollIndex += 2
+            else:
+                score += self.get_frame_score(frameIndex)
+                rollIndex +=2
+        return score
 
     def takedown(self, pins_number: int) -> None:
-        self._score += pins_number
+        self._rolls.append(pins_number)
